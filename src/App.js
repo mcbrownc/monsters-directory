0df485d65063acdbf5d4db043f022776bbe34e02
Corey McClendon-Brown <mcbrownc@hotmail.com>
2023-05-10 14:42:33 -0700

    Added search functionality

--- src/App.js
+++ src/App.js
@@ -7,10 +7,13 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: '',
     }
+    console.log('constructor');
   }
 
   componentDidMount() {
+    console.log('componentDidMount')
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json()) 
       .then((users) => 
@@ -25,18 +28,31 @@ class App extends Component {
       )
   }
 
+
+
   render() {
+    console.log('render')
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(this.state.searchField);
+    })
+    console.log(filteredMonsters)
+
+
     return (
       <div className="App">
-        {
-          this.state.monsters.map((monster) => {
-            return (
-            <div key={monster.id}>
-              <h1 >{monster.name}</h1>
-            </div>
-            )
+        <input className='search-box' type='search' placeholder='search monsters' onChange={(event) => {
+          const searchField = event.target.value.toLocaleLowerCase();
+          this.setState(() => {
+            return {searchField};
           })
-        }
+        }}/>
+        {filteredMonsters.map((monster) => {
+          return (
+          <div key={monster.id}>
+            <h1 >{monster.name}</h1>
+          </div>
+          )
+        })}
       </div>
     );
   }
