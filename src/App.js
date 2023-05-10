8e6fecb6542dd0a9da02c61a5312c286acf712a1
Corey McClendon-Brown <mcbrownc@hotmail.com>
2023-05-10 12:50:20 -0700

    Added map of state

--- src/App.js
+++ src/App.js
@@ -7,21 +7,31 @@ class App extends Component {
     super();
 
     this.state = {
-      name: 'Corey'
+      monsters: [
+        {
+          name: 'Linda'
+        },
+        {
+          name: 'Frank'
+        },
+        {
+          name: 'Jacky'
+        },
+        {
+          name: 'Andre'
+        },
+      ]
     }
-
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi {this.state.name}</p>
-          <button onClick={
-            this.setState({name: 'Andrew'})
-          }>Change Name</button>
-        </header>
+        {
+          this.state.monsters.map((monster) => {
+            return <h1>{monster.name}</h1>
+          })
+        }
       </div>
     );
   }
