3a0e5e7f5fdf05550152eaa4c191bc1e1906af04
Corey McClendon-Brown <mcbrownc@hotmail.com>
2023-05-11 16:27:26 -0700

    built out CardList

--- src/App.js
+++ src/App.js
@@ -6,25 +6,19 @@ import CardList from './components/card-list/card-list.component';
 class App extends Component {
   constructor() {
     super();
-
     this.state = {
       monsters: [],
       searchField: '',
     }
-    console.log('constructor');
   }
 
   componentDidMount() {
-    console.log('componentDidMount')
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json()) 
       .then((users) => 
         this.setState(
           () => {
             return {monsters: users};
-          },
-          () => {
-            console.log(this.state);
           }
         )
       )
@@ -38,28 +32,23 @@ class App extends Component {
   }
 
   render() {
-    console.log('render')
-
     const { monsters, searchField } = this.state;
     const { onSearchChange } = this;
 
     const filteredMonsters = monsters.filter((monster) => {
-      return monster.name.toLowerCase().includes(searchField);
+      return monster.name.toLocaleLowerCase().includes(searchField);
     })
 
 
     return (
       <div className="App">
-        <input className='search-box' type='search' placeholder='search monsters' onChange={onSearchChange}/>
-        {/* {filteredMonsters.map((monster) => {
-          return (
-          <div key={monster.id}>
-            <h1 >{monster.name}</h1>
-          </div>
-          )
-        })} */}
-        <CardList />
-        
+        <input 
+          className='search-box' 
+          type='search' 
+          placeholder='search monsters' 
+          onChange={onSearchChange}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,9 +2,13 @@ import { Component } from "react";
 
 class CardList extends Component {
   render() {
+    const { monsters } = this.props;
+
     return (
       <div>
-        Hello I'm the CardList Component!
+        {monsters.map((monster) => {
+          return <h1 key={monster.id}>{monster.name}</h1>
+        })}
       </div>
     )
   }
