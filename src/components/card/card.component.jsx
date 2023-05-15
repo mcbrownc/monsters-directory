857282df7f2101585992972738202601853b0ab2
Corey McClendon-Brown <mcbrownc@hotmail.com>
2023-05-14 20:28:57 -0700

    Coverted everything to functional components

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,17 +1,27 @@
-import { Component } from "react";
+// import { Component } from "react";
 import './card.styles.css';
 
-class Card extends Component {
-  render() {
-    const { name, id, email } = this.props;
-    return (
-      <div className='card-container'>
-        <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2`} />
-        <h2>{name}</h2>
-        <p>{email}</p>
-      </div>
-    )
-  }
+const Card = ({ name, id, email }) => {
+  return (
+    <div className='card-container'>
+      <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2`} />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  )
 }
+
+// class Card extends Component {
+//   render() {
+//     const { name, id, email } = this.props;
+//     return (
+//       <div className='card-container'>
+//         <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2`} />
+//         <h2>{name}</h2>
+//         <p>{email}</p>
+//       </div>
+//     )
+//   }
+// }
 
 export default Card;
