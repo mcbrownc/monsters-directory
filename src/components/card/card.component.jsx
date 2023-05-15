d4f4c81b2d3a39842a7bbb1144b36b58216661fa
Corey McClendon-Brown <mcbrownc@hotmail.com>
2023-05-14 17:22:34 -0700

    Added styling and Card component

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,23 @@
+import { Component } from "react";
+import './card.styles.css';
+
+class Card extends Component {
+  render() {
+    const { monsters } = this.props;
+    return (
+      <div className='card-list'>
+      {monsters.map((monster) => {
+        const { name, id, email } = monster;
+        return (
+          <div className='card-container' key={id}>
+            <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2`} />
+            <h2>{name}</h2>
+            <p>{email}</p>
+          </div>
+      )})}
+    </div>
+    )
+  }
+}
+
+export default Card;
